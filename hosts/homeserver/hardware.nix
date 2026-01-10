eb6e49d7765777fd0b9fcfa66e0db01c5b9a9641
Damien Pontifex <damien.pontifex@gmail.com>
2026-01-10 18:28:33 +0800

    Hardware from `nixos-generate-config --show-hardware-config`

--- hosts/homeserver/hardware.nix
+++ hosts/homeserver/hardware.nix
@@ -5,16 +5,10 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "nvme"
-    "usb_storage"
-    "sd_mod"
-  ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "ums_realtek" "usb_storage" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/REPLACE-ME";
@@ -27,4 +21,7 @@
   };
 
   swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault = "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
