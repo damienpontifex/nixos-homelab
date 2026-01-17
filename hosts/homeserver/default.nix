757c862502ec9c1f2cf98e5b1a24f6253696fbc0
Damien Pontifex <damien.pontifex@gmail.com>
2026-01-17 17:27:07 +0800

    standardise hardware-configuration naming

--- hosts/homeserver/default.nix
+++ hosts/homeserver/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ./hardware.nix
+    ./hardware-configuration.nix
     ./disk-config.nix
     ../../modules # Imports common modules from modules/default.nix
     # ../../modules/k3s.nix  # K3s is optional, imported per-host
