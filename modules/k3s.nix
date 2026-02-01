f22a94d41c48a213be5c23b3efdb47f9b9171379
Damien Pontifex <damien.pontifex@gmail.com>
2026-02-01 08:33:07 +0800

    Trimmed down to install cilium

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -95,10 +95,11 @@ in
       "--kube-apiserver-arg=anonymous-auth=true"
       "--kube-apiserver-arg=service-account-issuer=https://homelab.pontifex.dev"
       "--kube-apiserver-arg=service-account-jwks-uri=https://homelab.pontifex.dev/openid/v1/jwks"
-      "--flannel-backend=none" # Disable Flannel to use Cilium
-      "--disable-network-policy" # Let Cilium handle network policies
-      "--disable-kube-proxy" # Let Cilium handle kube-proxy functionality
       "--write-kubeconfig-mode=0664"
+      # Cilium
+      "--flannel-backend=none" # Disable Flannel to use Cilium
+      "--disable-kube-proxy" # Let Cilium handle kube-proxy functionality
+      "--disable-network-policy" # Let Cilium handle network policies
     ];
     disable = [
       "traefik"
@@ -146,134 +147,134 @@ in
       };
     };
 
-    autoDeployCharts.argocd = lib.mkIf (config.services.k3s.role == "server") {
-      name = "argocd";
-      # https://artifacthub.io/packages/helm/argo-cd-oci/argo-cd
-      repo = "oci://ghcr.io/argoproj/argo-helm/argo-cd";
-      # renovate: datasource=helm registryUrl=https://argoproj.github.io/argo-helm depName=argo-cd
-      version = "9.3.4";
-      hash = "sha256-dpTJFsJgs8rZU3ejxgyggLSpeYGGZnFTPLeQVMV0wG0=";
-      targetNamespace = "argocd";
-      createNamespace = true;
-      values = {
-        global = {
-          domain = "argocd.home.pontifex.dev";
-        };
-        server = {
-          ingress = {
-            enabled = true;
-            ingressClassName = "traefik";
-            tls = true;
-            annotations = {
-              "cert-manager.io/cluster-issuer" = "letsencrypt-prod";
-            };
-          };
-        };
-        configs = {
-          params = {
-            "server.insecure" = "true";
-          };
-          cm = {
-            "kustomize.buildOptions" = "--enable-helm";
-            "accounts.gethomepage" = "apiKey";
-          };
-          rbac = {
-            "policy.default" = "role:readonly";
-            "policy.csv" = ''
-              p, gethomepage, *, *, *, role:readonly
-            '';
-          };
-        };
-      };
-    };
-
-    # Bootstrap ArgoCD Application for Homelab
-    manifests = {
-      homelab-application = {
-        enable = true;
-        target = "homelab-application.yaml";
-        content = {
-          apiVersion = "argoproj.io/v1alpha1";
-          kind = "Application";
-          metadata = {
-            name = "homelab";
-            namespace = "argocd";
-          };
-          spec = {
-            project = "homelab";
-            source = {
-              repoURL = "https://github.com/damienpontifex/homelab";
-              path = "apps/";
-              directory = {
-                recurse = true;
-                include = "*/application.yaml";
-              };
-            };
-            destination = {
-              name = "in-cluster";
-              namespace = "default";
-            };
-            syncPolicy = {
-              automated = {
-                enabled = true;
-                prune = true;
-                selfHeal = true;
-              };
-              syncOptions = [
-                "ServerSideApply=true"
-                "CreateNamespace=true"
-              ];
-              retry = {
-                limit = 2;
-                backoff = {
-                  duration = "5s";
-                  factor = 2;
-                  maxDuration = "3m";
-                };
-              };
-            };
-          };
-        };
-      };
-      homelab-project = {
-        enable = true;
-        target = "homelab-project.yaml";
-        content = {
-          apiVersion = "argoproj.io/v1alpha1";
-          kind = "AppProject";
-          metadata = {
-            name = "homelab";
-            namespace = "argocd";
-          };
-          spec = {
-            description = "pontifex.dev Homelab";
-            # Allow manifests to deploy from any Git repos under damienpontifex GitHub account
-            sourceRepos = [
-              # "https://github.com/damienpontifex/*"
-              # "https://jameswynn.github.io/helm-charts" # homepage
-              # "https://charts.external-secrets.io"
-              # "https://kyverno.github.io/kyverno"
-              # "https://kubernetes.github.io/*"
-              "*"
-            ];
-            # Allow deployment to any namespace on the cluster
-            destinations = [
-              {
-                namespace = "*";
-                server = "*";
-              }
-            ];
-            clusterResourceWhitelist = [
-              {
-                group = "*";
-                kind = "*";
-              }
-            ];
-          };
-        };
-      };
-    };
-  };
+  #   autoDeployCharts.argocd = lib.mkIf (config.services.k3s.role == "server") {
+  #     name = "argocd";
+  #     # https://artifacthub.io/packages/helm/argo-cd-oci/argo-cd
+  #     repo = "oci://ghcr.io/argoproj/argo-helm/argo-cd";
+  #     # renovate: datasource=helm registryUrl=https://argoproj.github.io/argo-helm depName=argo-cd
+  #     version = "9.3.4";
+  #     hash = "sha256-dpTJFsJgs8rZU3ejxgyggLSpeYGGZnFTPLeQVMV0wG0=";
+  #     targetNamespace = "argocd";
+  #     createNamespace = true;
+  #     values = {
+  #       global = {
+  #         domain = "argocd.home.pontifex.dev";
+  #       };
+  #       server = {
+  #         ingress = {
+  #           enabled = true;
+  #           ingressClassName = "traefik";
+  #           tls = true;
+  #           annotations = {
+  #             "cert-manager.io/cluster-issuer" = "letsencrypt-prod";
+  #           };
+  #         };
+  #       };
+  #       configs = {
+  #         params = {
+  #           "server.insecure" = "true";
+  #         };
+  #         cm = {
+  #           "kustomize.buildOptions" = "--enable-helm";
+  #           "accounts.gethomepage" = "apiKey";
+  #         };
+  #         rbac = {
+  #           "policy.default" = "role:readonly";
+  #           "policy.csv" = ''
+  #             p, gethomepage, *, *, *, role:readonly
+  #           '';
+  #         };
+  #       };
+  #     };
+  #   };
+  #
+  #   # Bootstrap ArgoCD Application for Homelab
+  #   manifests = {
+  #     homelab-application = {
+  #       enable = true;
+  #       target = "homelab-application.yaml";
+  #       content = {
+  #         apiVersion = "argoproj.io/v1alpha1";
+  #         kind = "Application";
+  #         metadata = {
+  #           name = "homelab";
+  #           namespace = "argocd";
+  #         };
+  #         spec = {
+  #           project = "homelab";
+  #           source = {
+  #             repoURL = "https://github.com/damienpontifex/homelab";
+  #             path = "apps/";
+  #             directory = {
+  #               recurse = true;
+  #               include = "*/application.yaml";
+  #             };
+  #           };
+  #           destination = {
+  #             name = "in-cluster";
+  #             namespace = "default";
+  #           };
+  #           syncPolicy = {
+  #             automated = {
+  #               enabled = true;
+  #               prune = true;
+  #               selfHeal = true;
+  #             };
+  #             syncOptions = [
+  #               "ServerSideApply=true"
+  #               "CreateNamespace=true"
+  #             ];
+  #             retry = {
+  #               limit = 2;
+  #               backoff = {
+  #                 duration = "5s";
+  #                 factor = 2;
+  #                 maxDuration = "3m";
+  #               };
+  #             };
+  #           };
+  #         };
+  #       };
+  #     };
+  #     homelab-project = {
+  #       enable = true;
+  #       target = "homelab-project.yaml";
+  #       content = {
+  #         apiVersion = "argoproj.io/v1alpha1";
+  #         kind = "AppProject";
+  #         metadata = {
+  #           name = "homelab";
+  #           namespace = "argocd";
+  #         };
+  #         spec = {
+  #           description = "pontifex.dev Homelab";
+  #           # Allow manifests to deploy from any Git repos under damienpontifex GitHub account
+  #           sourceRepos = [
+  #             # "https://github.com/damienpontifex/*"
+  #             # "https://jameswynn.github.io/helm-charts" # homepage
+  #             # "https://charts.external-secrets.io"
+  #             # "https://kyverno.github.io/kyverno"
+  #             # "https://kubernetes.github.io/*"
+  #             "*"
+  #           ];
+  #           # Allow deployment to any namespace on the cluster
+  #           destinations = [
+  #             {
+  #               namespace = "*";
+  #               server = "*";
+  #             }
+  #           ];
+  #           clusterResourceWhitelist = [
+  #             {
+  #               group = "*";
+  #               kind = "*";
+  #             }
+  #           ];
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 
   # Automatically open firewall ports for k3s
   networking.firewall.allowedTCPPorts = lib.mkIf config.services.k3s.enable [
