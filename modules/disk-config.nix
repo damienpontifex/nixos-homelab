da26da03028cd307d4eb33332ed5843fed0f7888
Damien Pontifex <damien.pontifex@gmail.com>
2026-02-07 19:54:40 +0800

    Aligning disk config to nixos installer

--- modules/disk-config.nix
+++ modules/disk-config.nix
@@ -16,6 +16,20 @@
     };
   };
 
+# Equivalent manual partitioning (with no swap)
+# Partitioning (ESP first, then Root)
+# parted /dev/sda -- mklabel gpt
+# parted /dev/sda -- mkpart ESP fat32 1MB 513MB (This becomes /dev/sda1)
+# parted /dev/sda -- set 1 esp on
+# parted /dev/sda -- mkpart root ext4 513MB 100% (This becomes /dev/sda2)
+# Formatting & Labeling
+# mkfs.fat -F 32 -n boot /dev/sda1
+# mkfs.ext4 -L nixos /dev/sda2
+# Mounting with Security Flags
+# mount /dev/disk/by-label/nixos /mnt
+# mkdir -p /mnt/boot
+# mount -o umask=0077 /dev/disk/by-label/boot /mnt/boot
+
   config = {
     disko.devices = {
       disk = {
@@ -25,6 +39,7 @@
           content = {
             type = "gpt";
             partitions = {
+              # 512MiB boot partition at the start
               ESP = {
                 size = "512M";
                 type = "EF00";
@@ -35,7 +50,18 @@
                   mountOptions = [ "umask=0077" ]; # So only the root user can read/write
                 };
               };
-            } // lib.optionalAttrs config.diskConfig.useSwap {
+              # Root partition filling the middle
+              nixos = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            }
+            # 8GB swap partition at the end (if configured)
+              // lib.optionalAttrs config.diskConfig.useSwap {
               swap = {
                 size = "8G"; # Default matching RAM
                 content = {
@@ -44,16 +70,7 @@
                   resumeDevice = true; # Allows for hibernation support
                 };
               };
-            } // {
-              root = {
-                size = "100%";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
-              };
-            };
+            }
           };
         };
       };
