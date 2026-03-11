329c8b8f7a107ce547aee531906e27efaa423107
Damien Pontifex <damien.pontifex@gmail.com>
2026-03-11 19:18:39 +0800

    semicolons

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -175,9 +175,9 @@ in
     # Bootstrap ArgoCD Application for Homelab
     manifests = {
       # Source https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.5.0/standard-install.yaml
-      gateway-api.source = ./k3s-bootstrap/gateway-api-standard-install.yaml
-      homelab-application.source = ./k3s-bootstrap/argocd-bootstrap.yaml
-      cloudflare-namespace.source = ./k3s-bootstrap/cloudflare.yaml
+      gateway-api.source = ./k3s-bootstrap/gateway-api-standard-install.yaml;
+      homelab-application.source = ./k3s-bootstrap/argocd-bootstrap.yaml;
+      cloudflare-namespace.source = ./k3s-bootstrap/cloudflare.yaml;
       cloudflare-token-secret = {
         target = "cloudflare-token-secret.yaml";
         # Use the sops template instead of trying to read the file directly
